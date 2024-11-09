c212d86a6cfb7af81317ad184920b6146b23d942
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2024-11-09 16:32:41 +0700

    membuat db.js, tododb.js, env

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const todoRoutes = require('./routes/todo.js');
-const port = 5000;
+const todoRoutes = require('./routes/tododb.js');
+//const port = 3000;
+
+require('dotenv').config();
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use('/todos', todoRoutes);
